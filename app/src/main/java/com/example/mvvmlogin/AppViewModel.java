2a290898005c93c13a1d6033e7d8c90c3896178f
mits <devanshu18g@gmail.com>
2021-05-07 18:33:20 +0530

    100% Done Testing Part Not Done

---
+++ app/src/main/java/com/example/mvvmlogin/AppViewModel.java
@@ -0,0 +1,67 @@
+package com.example.mvvmlogin;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import java.util.regex.Pattern;
+
+public class AppViewModel extends BaseObservable {
+
+private Model model;
+
+private String sucessMessage ="Login Successful";
+private String errorMessage ="Email or Password Not Valid";
+
+@Bindable
+private String toastMessage = null;
+
+    public String getToastMessage() {
+
+        return toastMessage;
+    }
+
+    public void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    @Bindable
+    public String getUserEmail(){
+        return model.getEmail();
+    }
+
+    @Bindable
+    public void setUserEmail(String email){
+        model.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+    @Bindable
+    public String getUserPassword(){
+        return model.getPassword();
+    }
+    @Bindable
+    public void setUserPassword(String password){
+        model.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public AppViewModel() {
+        model = new Model("","");
+    }
+    public void onButtonClicked()
+    {
+        if(isValid())
+            setToastMessage(sucessMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+    public boolean isValid()
+    {
+        return !TextUtils.isEmpty(getUserEmail()) &&
+                Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches()
+                && getUserPassword().length()>5;
+    }
+}
--- app/src/main/java/com/example/mvvmlogin/MainActivity.java
+++ app/src/main/java/com/example/mvvmlogin/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mvvmlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.mvvmlogin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(new AppViewModel());
+        activityMainBinding.executePendingBindings();
+//        setContentView(R.layout.activity_main);
+    }
+    @BindingAdapter({"toastMessage"})
+    public static void  runme(View view, String message){
+    if(message!=null)
+        Toast.makeText(view.getContext(),message, Toast.LENGTH_SHORT).show();
     }
 }
